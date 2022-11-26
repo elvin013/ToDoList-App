b0a0dee28622dab1d12747a407d5ff46be57ac00
Elvis Pouriet <frainerdeveloper@gmail.com>
2022-11-26 18:09:58 -0400

    [FEAT] add the assets features to the project

--- public/assets/js/layoutScript.js
+++ public/assets/js/layoutScript.js
@@ -9,6 +9,7 @@ closeBtn.addEventListener("click", () => {
   menuBtnChange(); //calling the function(optional)
 });
 
+
 searchBtn.addEventListener("click", () => {
   // Sidebar open when you click on the search iocn
   sidebar.classList.toggle("open");
