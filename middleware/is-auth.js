92415e33c7ef52207a9fb061056e19f346948787
Elvis Pouriet <frainerdeveloper@gmail.com>
2022-11-26 17:55:52 -0400

    [FIX] fix the error message to return in the request flash

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -2,7 +2,7 @@
 home page and display the message: "Your session has expired, you must log in again"  */
 module.exports = (req, res, next) => {
   if (!req.session.isLoggedIn) {
-     req.flash("errors", "Your session has expired");
+     req.flash("errors", "Your session has expired, you must log in again");
     return res.redirect("/home");
   }
 
